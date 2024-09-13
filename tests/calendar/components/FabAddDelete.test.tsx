4f529d47c750dd2de54d750dcdcf5e762caa4e58
Edinson <94578362+edinsondevs@users.noreply.github.com>
2024-09-12 23:07:07 -0300

    corrigiendo error en fabdelete

--- tests/calendar/components/FabAddDelete.test.tsx
+++ tests/calendar/components/FabAddDelete.test.tsx
@@ -1,16 +1,11 @@
 import { render, screen } from "@testing-library/react";
-import { Navbar } from "../../../src/calendar";
-// import { Navbar } from "../../../src/calendar/components/Navbar";
-// import { FabAddDelete } from "../../../src/calendar/components/FabAddDelete";
+import { FabAddDelete } from "../../../src/calendar/components/FabAddDelete";
 
 describe('pruebas en el FabAddDelete', () => {
     
     test('debe de mostrar el componente', () => {
         
-        // render(<FabAddDelete />);
-        render(
-            <Navbar />
-        )
+        render(<FabAddDelete />);
         screen.debug();
 
     });
